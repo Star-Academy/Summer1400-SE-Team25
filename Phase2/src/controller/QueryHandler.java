3ecd82481a2a5a1ead4e50dc019b7e6ce53b9ac9
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-07-26 14:46:02 +0430

    Delete sorting queries

--- Phase2/src/controller/QueryHandler.java
+++ Phase2/src/controller/QueryHandler.java
@@ -20,28 +20,10 @@ public class QueryHandler {
 
     public HashSet<DocumentFile> search(String query) {
         setQueryList(query);
-        sortQueryList();
         HashSet<DocumentFile> res = new HashSet<>(invertedIndex.getDocuments());
         for(Query i : queriesList)
             res = i.pushSearchResult(res);
         return res;
-    }
-
-    /**
-     * sort the queries list so that every ANDQuery will be computed before every ORQuery
-     * and every ORQuery will be computed before every NOTQuery
-     */
-    private void sortQueryList() {
-        Collections.sort(queriesList, (o1, o2) -> {
-            if (o1 instanceof ANDQuery) // o1 is ANDQuery
-                return (o2 instanceof ANDQuery? 0: -1);
-            else if (o2 instanceof ANDQuery) // o2 is ANDQuery and o1 either is ORQuery or NOTQuery
-                return 1;
-            else if (o1 instanceof ORQuery) // o1 is ORQuery and o2 isn't ANDQuery
-                return (o2 instanceof ORQuery? 0: -1);
-            else // o1 is NOTQuery
-                return (o2 instanceof NOTQuery? 0: 1);
-        });
     }
 
     private void setQueryList(String query) {
