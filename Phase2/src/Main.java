9192277687184f98d765d940609fafdf9aa85049
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-07-26 12:51:27 +0430

    Complete SearchEngine

--- Phase2/src/Main.java
+++ Phase2/src/Main.java
@@ -3,6 +3,6 @@ import view.SearchEngine;
 public class Main {
     public static void main(String[] args) {
         SearchEngine searchEngine = new SearchEngine();
-        
+        searchEngine.start();
     }
 }
--- Phase2/src/view/SearchEngine.java
+++ Phase2/src/view/SearchEngine.java
@@ -2,15 +2,37 @@ package view;
 
 import controller.FileHandler;
 import controller.QueryHandler;
+import model.DocumentFile;
 import util.Config;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class SearchEngine {
-    private FileHandler fileHandler;
-    private QueryHandler queryHandler;
+    private final FileHandler fileHandler;
+    private final QueryHandler queryHandler;
+    private final Scanner inputScanner;
 
     public SearchEngine() {
         fileHandler = new FileHandler(Config.retrieveProperty("ENGLISH_DATA_PATH"));
         queryHandler = new QueryHandler(fileHandler.getInvertedIndex());
-        queryHandler.run();
+        inputScanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        System.out.println("Enter some words, separated by space: ");
+        String input = inputScanner.nextLine();
+        HashSet<DocumentFile> resultSet = queryHandler.search(input);
+        showResults(resultSet);
+        inputScanner.close();
+
+    }
+
+    private void showResults(HashSet<DocumentFile> resultSet) {
+        System.out.println("Results: ");
+        for (DocumentFile document : resultSet) {
+            System.out.println("\t" + document.getFileName());
+            System.out.println("\t\t" + document.previewDocument());
+        }
     }
 }
