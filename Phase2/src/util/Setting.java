c59d0107ed4f2524e7d3187f3e4486e6a6e0777c
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-07-26 11:40:43 +0430

    Modified Setting file

--- Phase2/src/util/Setting.java
+++ Phase2/src/util/Setting.java
@@ -2,4 +2,5 @@ package util;
 
 public class Setting {
     public final static String configFileAddress = "src/Config.properties";
+    public final static int documentPreviewCharacterCount = 27;
 }
