46b64c184d09398ae4e195e265b57805041eaae9
SamanEN <SamanEslami78@gmail.com>
2021-07-31 11:42:59 +0430

    refactor

--- Phase2/src/main/java/Main.java
+++ Phase2/src/main/java/Main.java
@@ -5,10 +5,11 @@ import model.InvertedIndex;
 import view.UserView;
 
 public class Main {
+    final static String DEFAULT_PATH = "src/main/java/EnglishData";
     public static void main(String[] args) {
         var invertedIndex = new InvertedIndex();
         var fileHandler = new FileHandler(invertedIndex);
-        fileHandler.initialize();
+        fileHandler.initialize(DEFAULT_PATH);
         var queryHandler = new QueryHandler(invertedIndex);
         var searchEngine = new SearchEngine(queryHandler);
         var userView = new UserView(searchEngine);
