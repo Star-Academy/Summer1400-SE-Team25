6b0ad0c983752a87642b5ec8f2a4721be90e3f3a
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-08-01 12:40:44 +0430

    Complete tests

--- Phase2/src/test/java/MainTest.java
+++ Phase2/src/test/java/MainTest.java
@@ -1,2 +1,10 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MainTest {
+    @Test
+    public void testMainMethod() {
+    }
 }
--- Phase2/src/test/java/utilTest/WordUtilTest.java
+++ Phase2/src/test/java/utilTest/WordUtilTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import util.WordUtil;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WordUtilTest {
