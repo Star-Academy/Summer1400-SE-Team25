8f0139918f81cca794f22fb788ddf63bb25fd853
SamanEN <SamanEslami78@gmail.com>
2021-08-02 17:40:14 +0430

    init project

---
+++ Phase4/Students Grade and Average/Students Grade and Average/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Students_Grade_and_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
