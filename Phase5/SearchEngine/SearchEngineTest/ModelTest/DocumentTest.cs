34e957033edab3f823162da6eea2fbb5e5fe7329
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-08-03 17:20:21 +0430

    Complete Document tests

--- Phase5/SearchEngine/SearchEngineTest/ModelTest/DocumentTest.cs
+++ Phase5/SearchEngine/SearchEngineTest/ModelTest/DocumentTest.cs
@@ -9,8 +9,9 @@ namespace SearchEngineTest.ModelTest
 {
     public class DocumentTest
     {
-        private const string ValidEnglishDataPath = "EnglishData/57110";
-        private const string InvalidEnglishDataPath = "EnglishData/100";
+        private const string ValidDocumentPath = "EnglishData/57110";
+        private const string InvalidDocumentPath = "EnglishData/100";
+        private const string ValidDocumentPreview = "I have a 42 yr old male fri...";
         private Document _document;
 
         public DocumentTest()
@@ -19,32 +20,35 @@ namespace SearchEngineTest.ModelTest
 
         private void InitializeValidDocument()
         {
-            _document = new Document(ValidEnglishDataPath);
+            _document = new Document(ValidDocumentPath);
         }
 
         private void InitializeInvalidDocument()
         {
-            _document = new Document(InvalidEnglishDataPath);
+            _document = new Document(InvalidDocumentPath);
         }
 
         [Fact]
-        public void MakeValidDocument()
+        public void TestValidDocument()
         {
             InitializeValidDocument();
+            var exception = Record.Exception(_document.GetDocumentPreview);
+            Assert.Null(exception);
         }
 
         [Fact]
-        public void MakeInvalidDocument()
+        public void TestInvalidDocument()
         {
             InitializeInvalidDocument();
-            // Assert.Throws<FileNotFoundException>(_document.GetDocumentName);
+            Assert.Throws<FileNotFoundException>(_document.GetDocumentPreview);
         }
 
         [Fact]
-        public void GetDocumentNameTest()
+        public void TestDocumentPreview()
         {
             InitializeValidDocument();
-            var documentName = _document.GetDocumentName();
+            var documentName = _document.GetDocumentPreview();
+            Assert.Equal(ValidDocumentPreview, documentName);
         }
     }
 }
