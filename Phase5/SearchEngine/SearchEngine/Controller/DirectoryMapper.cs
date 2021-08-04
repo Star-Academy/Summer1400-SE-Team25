d7a2f5e83f66dafc6fb70246ae05650110dd3e87
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-08-04 16:06:10 +0430

    Complete DirectoryMapper

--- Phase5/SearchEngine/SearchEngine/Controller/DirectoryMapper.cs
+++ Phase5/SearchEngine/SearchEngine/Controller/DirectoryMapper.cs
@@ -1,12 +1,25 @@
+using System.Collections.Generic;
+using System.IO;
 using SearchEngine.Model;
 
 namespace SearchEngine.Controller
 {
     public class DirectoryMapper : IDirectoryMapper
     {
-        public Document GetDocumentByDirectory(string directory)
+        private IDictionary<string, Document> _directoryMapper;
+
+        public DirectoryMapper()
         {
-            throw new System.NotImplementedException();
+            _directoryMapper = new Dictionary<string, Document>();
+        }
+
+        public Document GetDocumentByDirectory(string documentDirectory)
+        {
+            if (!File.Exists(documentDirectory))
+                return null;
+            if (!_directoryMapper.ContainsKey(documentDirectory))
+                _directoryMapper.Add(documentDirectory, new Document(documentDirectory));
+            return _directoryMapper[documentDirectory];
         }
     }
 }
