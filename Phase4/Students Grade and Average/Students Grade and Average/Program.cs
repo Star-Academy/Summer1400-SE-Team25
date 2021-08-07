d1fab298d2740966a2b2dd8a196a781e95a94195
SamanEN <SamanEslami78@gmail.com>
2021-08-07 11:57:57 +0430

    Refactor

--- Phase4/Students Grade and Average/Students Grade and Average/Program.cs
+++ Phase4/Students Grade and Average/Students Grade and Average/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-
+using System.IO;
 namespace StudentsGradeandAverage
 {
     class Program
     {
+        private const string StudentsFilePath = "Students.json";
+        private const string GradesFilePath = "Grades.json";
         static void Main(string[] args)
         {
-            var parser = new Parser("Students.json", "Grades.json");
-            parser.Parse();
+            var parser = new Parser();
+            parser.Parse(new StreamReader(StudentsFilePath), new StreamReader(GradesFilePath));
             var studentsScoreHandler = new StudentsScoreHandler(parser.Students, parser.Grades);
             studentsScoreHandler.AssignGradesToStudents();
             studentsScoreHandler.SortStudentsByScore();
